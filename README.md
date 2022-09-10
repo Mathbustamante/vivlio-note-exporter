197b91199522dd92da75fab771e077b69ae8f3cf
mathbustamante <matheusbustamante24@gmail.com>
2022-09-10 18:43:32 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Export Vivlio Book Notes to Notion
+
+<img src="https://i.ibb.co/tMGmJjw/Captura-de-Tela-2022-09-10-a-s-5-43-50-PM.png" alt="drawing" width="1000"/>
+
+## About the Integration
+
+This Notion integration sends book notes taken in a Vivlio e-book reader to a particular Notion database.
+
+## Running Locally
+
+### 1. Setup your local project
+
+```zsh
+# Clone this repository locally
+git clone https://github.com/Mathbustamante/vivlio-note-exporter.git
+
+# Switch into this project
+cd vivlio-note-exporter
+
+# Install the dependencies
+npm install
+```
+
+### 2. Set your environment variables in a `.env` file
+
+```zsh
+NOTION_KEY= <your-notion-api-key>
+NOTION_DATABASE_ID=<your-notion-database-id>>
+```
+
+You can create your Notion API key [here](https://www.notion.com/my-integrations).
+
+To create a Notion database that will work with this example, duplicate [this template](https://adaptive-pike-454.notion.site/e2a64f72349344b88157d5ecbd7c81d3?v=6d2e5230670943419257e8d4519d0279).
+
+### 3 Go to your Notion database and add a new connection
+
+Add the connection linked to the Notion API key created on step 2.
+
+### 4. Set your book notes in a folder called `notes`
+
+Export your notes in Vivlio and past the generated `html` files to your `notes` folder.
+
+### 5. Run code
+
+```zsh
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,116 @@
+/* 
+	Export Book Notes - Vivlio
+*/
+
+const fs = require('fs');
+const jsdom = require("jsdom");
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const { Client } = require("@notionhq/client")
+const { JSDOM } = jsdom;
+
+const notion = new Client({
+  auth: process.env.NOTION_KEY
+});
+
+/**
+ * Starts Notion exporter
+ */
+startExport()
+
+/**
+ * Traverse each html file and returns array of objects containing the book title, author, and
+ * user notes
+ */
+async function getBookNotes() {
+  try {
+    const files = fs.readdirSync('./notes')
+    const bookNotes = [];
+    for (const book of files) {
+      const data = fs.readFileSync(`./notes/${book}`, 'utf8');
+      const dom = new JSDOM(data);
+
+      const currentBook = {};
+      currentBook.title = (dom.window.document.querySelector("h1").textContent).slice(22);
+      currentBook.author = dom.window.document.querySelector("span").textContent;
+      currentBook.notes = [];
+      dom.window.document.querySelectorAll(".bm-text").forEach(function (data) {
+        currentBook.notes.push({
+          object: "block",
+          type: "paragraph",
+          paragraph: {
+            rich_text: [{
+              type: "text",
+              text: {
+                content: `${data.parentNode.querySelector('.bm-page').textContent} - ${data.querySelector('.bm-text > *').textContent}`
+              }
+            }]
+          }
+        });
+      });
+      bookNotes.push(currentBook);
+    }
+    return bookNotes;
+  } catch (error) {
+    console.error(error)
+    throw new Error('There was an error reading your book notes.')
+  }
+}
+
+/**
+ * Gets a random book emoji
+ */
+ const getRandomEmoji = () => {
+  const bookEmojis = ['📔', '📕', '📖', '📗', '📘', '📙', '📚', '📓', '📒']
+  return bookEmojis[Math.floor((Math.random() * bookEmojis.length))]
+}
+
+/**
+ * Sends book notes to Notion
+ */
+async function publishBookNotes(bookName, bookAuthor, pageContent) {
+  try {
+    const response = await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_DATABASE_ID
+      },
+      icon: {
+        type: "emoji",
+        emoji: getRandomEmoji()
+      },
+      properties: {
+        title: {
+          title: [{
+            "text": {
+              "content": bookName
+            }
+          }]
+        },
+        Author: {
+          rich_text: [{
+            "text": {
+              "content": bookAuthor
+            }
+          }]
+        }
+      },
+      children: pageContent
+    })
+    console.log(response);
+    console.log(`Success! Entry added ${bookName}.`)
+  } catch (error) {
+    console.error(error.body)
+  }
+}
+
+/**
+ * Publish book notes to Notion
+ */
+async function startExport() {
+  const books = await getBookNotes();
+  for (let i = 0; i < books.length; i++) {
+    publishBookNotes(books[i].title, books[i].author, books[i].notes);
+  }
+}
